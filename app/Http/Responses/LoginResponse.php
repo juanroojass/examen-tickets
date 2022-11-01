82901dab5f18375b43f2d234d319b5f4b21e90e7
Juan Rojas <juan.roojass@gmail.com>
2022-11-01 13:12:15 -0600

    1.1

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -12,7 +12,7 @@ class LoginResponse implements LoginResponseContract
      */
     public function toResponse($request)
     {
-        $message = __('The provided two factor authentication code was invalidX test10.');
+        $message = __('The provided two factor authentication code was invalid test2.');
 
         if ($request->wantsJson()) {
             throw ValidationException::withMessages([
